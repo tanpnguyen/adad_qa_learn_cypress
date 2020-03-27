76e837e354a8799f5c0999165750a4245ff7f7ad
a.nguyenthivan <anhtvnguyen@kms-technology.com>
2020-03-27 12:31:17 +0700

    no message

--- src/pages/moduleObject.js
+++ src/pages/moduleObject.js
@@ -19,18 +19,40 @@ class module extends basePage {
     }
 
     pressCreateNewBtn = () => {
-        cy
-            .get(moduleSelectors.iconNewModule, { timeout: 30000 })
-            .click({ force: true })
-        cy
-            .get(moduleSelectors.txtEditTitle)
-            .should('be.visible')
-        cy
-            .get(moduleSelectors.txtEditTitle)
-            .should($title => {
-                expect($title).to.contain('Untitled')
-            })
+    cy
+        .get(moduleSelectors.projectRoot)
+        .click()
+    cy
+        .wait(10)   
+    cy
+        .get(moduleSelectors.iconNewModule, { timeout: 30000 })
+        .click()       
+            
     }
-   
+    // getURLParameter = (sParam) => {
+    //     let sPageURL = cy.url();
+    //     let sURLVariables = sPageURL.split('&');
+    //     for (var i = 0; i < sURLVariables.length; i++) {
+    //         var sParameterName = sURLVariables[i].split('=');
+    //         if (sParameterName[0] == sParam) {
+    //             return sParameterName[1];
+    //         }
+    //     }
+    // }
+    updateModuleName = (moduleName) => {
+        //let moduleId = this.getURLParameter('id')
+       // nodeModule = 'requirement-tree-expand-0-' + moduleId;
+    Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+            cy 
+            .get(moduleSelectors.txtEditTitle)
+            .click()
+            .wait(5)
+            .click()
+            .type (moduleName)
+            return false
+    })
+    }   
 }
 export default module
--- src/tests/module.js
+++ src/tests/module.js
@@ -14,8 +14,20 @@ describe('qTest Manager Module', () => {
     it('Click New Module button', () => {      
 
        moduleObj.pressCreateNewBtn();
+       moduleObj.updateModuleName('Automation Module');
 
     });
+
+    //it('Update name, assert new moodule created by clicking on the Reload button', () => {
+        
+       // moduleObj.updateModuleName('ADAD New Module');
+        // cy
+        //     .wrap(admin.getCreatedProjectId('ADAD_Cypress_Projects'))
+        //     .then(() => {
+        //         admin.navigateToCreatedProject(admin.projectId)
+        //     })
+
+   // });
 
 //     it('Delete test project',()=>{
 //         admin.deleteProject('ADAD_Cypress_Projects')
